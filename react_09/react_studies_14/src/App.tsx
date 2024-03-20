d629f716c60d52b223046eee2cd216d5b508aecd
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2024-03-20 10:08:31 -0300

    Criando a lista

---
+++ react_09/react_studies_14/src/App.tsx
@@ -0,0 +1,16 @@
+// import React from "react";
+import Botao from "./components/Botao";
+import Formulario from "./components/Formulario";
+import Lista from "./components/Lista";
+
+// --- Principal ---
+function App() {
+  return (
+    <>
+      <Lista />
+      <Formulario />
+    </>
+  );
+}
+
+export default App;
---
+++ react_09/react_studies_14/src/components/Lista/index.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+// --- function component ---
+function Lista() {
+  return (
+    <aside>
+        <h2> Estudos do dia </h2>
+        <ul>
+            <li>
+                <h3>React</h3>
+                <span>02:00:00</span>
+            </li>
+        </ul>
+        <ul>
+            <li>
+                <h3>JavaScript</h3>
+                <span>01:00:00</span>
+            </li>
+        </ul>
+    </aside>
+  )
+}
+
+export default Lista;
+
+
